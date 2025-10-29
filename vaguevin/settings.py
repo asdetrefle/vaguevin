0cd315b2c5aff6ae84a107d76b7f06602746506e
qjchv <qijie.chu@Albatros>
2025-10-29 12:21:54 +0800

    hide secret key

--- vaguevin/settings.py
+++ vaguevin/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6&j3ax_&w8a*ys1!4r1tsfa44-q+0uhuj&7%52ff*-w+s*sj9k"
+SECRET_KEY = config("SECRET_KEY",""),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
